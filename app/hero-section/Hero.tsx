b13ac354e315bb14e2ac20daa39cc37653ab92b3
ThenG <kimtheng324283@gmail.com>
2024-10-16 13:47:24 +0700

    Update Hero.tsx

--- app/hero-section/Hero.tsx
+++ app/hero-section/Hero.tsx
@@ -89,7 +89,7 @@ const Hero = () => {
           className={`relative flex flex-col items-center justify-center ${monaSans.className}`}
         >
           <AnimatedWords
-            title="VICTOR WILLIAMS"
+            title="KavTheng"
             style="inline-block overflow-hidden pt-1 -mr-4 sm:-mr-5 md:-mr-7 lg:-mr-9 -mb-1 sm:-mb-2 md:-mb-3 lg:-mb-4"
           />
           <motion.div
